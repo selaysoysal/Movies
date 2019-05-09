ae8d1bc426f71029e62215980e458fba7ea1411a
Selay Soysal <selaysoysal@gmail.com>
2019-05-09 16:45:35 +0300

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Selay Soysal on 9.05.2019.
+//  Copyright © 2019 Selay Turkmen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
